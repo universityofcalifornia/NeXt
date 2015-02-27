0b38edc5831c27708ddef56ed58ee6c0e81bb8b7
Eric Bollens <eric@eb.io>
2015-02-26 17:23:19 -0800

    Fix #2 - Complete ES workflow

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,30 @@
 class SearchController < ApplicationController
 
   TYPE_MAP = {
-    'ideas' => Idea,
-    'projects' => Project,
-    'users' => User
+    'ideas' => {
+        :class => Idea,
+        :type_name => 'Idea',
+        :title_method => :name,
+        :description_proc => Proc.new(){ |idea| idea.pitch }
+    },
+    'projects' => {
+        :class => Project,
+        :type_name => 'Project',
+        :title_method => :name,
+        :description_proc => Proc.new(){ |project| project.pitch }
+    },
+    'users' => {
+        :class => User,
+        :type_name => 'Person',
+        :title_method => :display_name,
+        :description_proc => Proc.new(){ |user|
+          if user.primary_position
+            "#{user.primary_position.title}, #{user.primary_position.department} ( #{user.primary_position.organization.shortname} )"
+          else
+            nil
+          end
+        }
+    }
   }
 
   def default
@@ -28,21 +49,34 @@ class SearchController < ApplicationController
 
     index_name = Rails.application.config.elasticsearch_config[:index_name]
 
-    res = Rails.application.config.elasticsearch_client.search index: index_name,
-                                                               body: search_body
+    search_properties = {
+      index: index_name,
+      body: search_body
+    }
 
-    results = res['hits']['hits'].map { |r|
+    if params[:type] and params[:type] != 'all'
+      search_properties[:type] = params[:type]
+    end
+
+    res = Rails.application.config.elasticsearch_client.search search_properties
+
+    @results = res['hits']['hits'].map { |r|
       begin
-      {
-        :object => TYPE_MAP[r['_type']].find(r['_id']),
-        :score => r['_score']
-      }
-      rescue
+        model = TYPE_MAP[r['_type']][:class].find(r['_id'])
+        OpenStruct.new({
+          :model => model,
+          :title => model.send(TYPE_MAP[r['_type']][:title_method]),
+          :type => TYPE_MAP[r['_type']][:type_name],
+          :score => r['_score'],
+          :description => TYPE_MAP[r['_type']][:description_proc].call(model)
+        })
+      rescue => e
+        puts e
         nil
       end
     }.delete_if { |o| o.nil? }
 
-    render plain: results
+    render 'results'
 
   end
 
