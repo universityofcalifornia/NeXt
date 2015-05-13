8dd6697fb8c2b40b2e544e2bc38cb41f0b7bc60b
Chris Habgood <chabgood@gmail.com>
2015-05-13 13:03:27 -0700

    capybara will not render the root page due to ssl,  event index page works

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+feature "Log in" do
+
+	background do
+    FactoryGirl.create(:user)
+  end
+
+  scenario "User visits secret page to log in" do
+
+    visit 'http://localhost:8080/auth/local/new'
+    save_and_open_page
+    #click_link 'Login'
+    #fill_in "Email", :with => "new"
+    #fill_in "Password", :with => "password"
+    #click_button "commit"
+
+    #expect(page).to have_text("Widget was successfully created.")
+  end
+end
