29a71af4a7851589aafc629ac08a0a325a96f4f6
Chris Habgood <chabgood@gmail.com>
2015-05-21 16:43:03 -0700

    fixed... event spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
 describe Event do
+  context "invalid url's" do
 
-  before(:all) do
-    @event  = FactoryGirl.create(:event)
+    it "should validate map_url if present" do
+      bad_map_url  = FactoryGirl.build(:event, :map_url => 'moo')
+      expect(bad_map_url).to be_invalid
+    end
+
+    it "should validate event_url if present" do
+      bad_event_url  = FactoryGirl.build(:event, :event_url => 'moo')
+      expect(bad_event_url).to be_invalid
+    end
   end
 
-  xit "should validate map_url if present" do
-    @event.map_url = nil
-    p @event
-    expect{ @event.save! }.to raise_error
+  context "validations and relationships" do
+  	it { should belong_to(:user) }
+    it { should validate_presence_of(:name) }
   end
-
-	it { should belong_to(:user) }
-  it { should validate_presence_of(:name) }
-
 
 end
