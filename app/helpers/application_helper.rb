bd2a3ddb571e1738b3088ec4438440f5d90d34d1
Chris Habgood <chabgood@gmail.com>
2015-05-26 12:32:40 -0700

    implemented from railscasts a marker to show required fields, WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,10 +15,10 @@ module ApplicationHelper
 
 
   def bootstrap_class_for flash_type
-   { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
- end
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  end
 
- def flash_messages(opts = {})
+  def flash_messages(opts = {})
    flash.each do |msg_type, message|
      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
        concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }) do
@@ -30,5 +30,9 @@ module ApplicationHelper
      flash.clear
    end
    nil
- end
+  end
+
+  def mark_required(object, attribute)
+    "*" if object.class.validators_on(attribute).map(&:class).include? ActiveModel::Validations::PresenceValidator
+  end
 end
