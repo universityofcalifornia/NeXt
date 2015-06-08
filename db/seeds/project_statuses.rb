ecd6d3454dc1170600bed6a10181ee8d653ec533
Keith Rozett <krozett@gmail.com>
2015-06-08 14:48:20 -0700

    Issue #69 - Update project statuses, remove non-ASCII char from seed data

--- db/seeds/project_statuses.rb
+++ db/seeds/project_statuses.rb
@@ -4,12 +4,10 @@ logger.info 'Truncate'
 ProjectStatus.truncate
 
 [
-  { key: 'proposal',    name: 'Proposal',    icon: '' },
-  { key: 'analysis',    name: 'Analysis',    icon: '' },
-  { key: 'prototype',   name: 'Prototype',   icon: '' },
-  { key: 'development', name: 'Development', icon: '' },
-  { key: 'rollout',     name: 'Rollout',     icon: '' },
-  { key: 'production',  name: 'Production',  icon: '' }
+  { key: 'forming',   name: 'Forming - recruiting resources', icon: '' },
+  { key: 'active',    name: 'Active - fully resourced',       icon: '' },
+  { key: 'completed', name: 'Successfully completed',         icon: '' },
+  { key: 'abandoned', name: 'Abandoned',                      icon: '' }
 ].each do |status|
   logger.info "Create - #{status[:key]} => #{status[:name]}"
   ProjectStatus.create status
