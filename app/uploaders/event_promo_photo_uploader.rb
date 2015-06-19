ad259a96e4ebba30a03d9d63d4990593d29b730b
Sandon Jurowski <jurowski@gmail.com>
2015-06-19 10:42:11 -0700

    commenting out rmagick references for ucnext.org server deploy

--- app/uploaders/event_promo_photo_uploader.rb
+++ app/uploaders/event_promo_photo_uploader.rb
@@ -3,7 +3,11 @@
 class EventPromoPhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  ### Sandon - commenting out for now for deploying to CentOS
+  ### re-enable once ImageMagick-devel works on CentOS v6
+  #include CarrierWave::RMagick
+ 
+
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
