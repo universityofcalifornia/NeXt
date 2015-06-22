363039e1d2ea9b24b4ba66f73c6c6cf579942cb8
Keith Rozett <krozett@gmail.com>
2015-06-22 15:05:04 -0700

    Issue #77 - Add support for Bootstrap's glyphicons

--- Blockfile.rb
+++ Blockfile.rb
@@ -50,6 +50,7 @@ block 'next', :path => BLOCKS_PATH do |n|
     dependency framework.route 'bootstrap', 'tables'
     dependency framework.route 'bootstrap', 'alerts'
     dependency framework.route 'bootstrap', 'list-group'
+    dependency framework.route 'bootstrap', 'glyphicons'
     dependency framework.route 'bootstrap', 'js', 'collapse'
     dependency framework.route 'bootstrap', 'js', 'dropdown'
     dependency framework.route 'bootstrap', 'js', 'modal'
