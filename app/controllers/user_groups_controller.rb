03511b2026841f55bed325f5652c86d5b0e357b2
Chris Habgood <chabgood@gmail.com>
2015-06-23 18:07:47 -0700

    added a delete link for each group the user belongs to

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,6 +1,11 @@
 class UserGroupsController < ApplicationController
 
   def create
-    UserGroup.where(:user_id => params[:user_id], :group_id => params[:id]).first_or_create
+    current_user.user_groups.where( :group_id => params[:id]).first_or_create
+  end
+
+  def destroy
+    user_group = current_user.user_groups.where(:group_id => params[:group_id]).first
+    user_group.destroy
   end
 end
