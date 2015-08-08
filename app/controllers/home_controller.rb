57b39faa0760198dc7391b01a6b8d8bf2a4dfe60
Eric Bollens <eric@eb.io>
2015-08-08 16:07:28 -0700

    Fix bug with social feed - whatever that is

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    # TODO: get real data for this
-    Struct.new("SocialFeedItem", :avatar, :author_handle, :author_name, :author_url, :url, :content, :time, :icon_class)
-    @social_feed = []
-    @social_feed.push Struct::SocialFeedItem.new(
-      "/images/blank_avatar.png",
-      "Joe Bruin",
-      "Joe Bruin",
-      "#",
-      "#",
-      "NeXt is still the best!",
-      "5 minutes ago",
-      "facebook"
-    )
-    @social_feed.push Struct::SocialFeedItem.new(
-      "/images/blank_avatar.png",
-      "@joebruin",
-      "Joe Bruin",
-      "#",
-      "#",
-      "NeXt is the best!",
-      "yesterday",
-      "twitter"
-    )
 
     return welcome
     # TODO: differentiate between welcome and dashboard once we have a dashboard design
