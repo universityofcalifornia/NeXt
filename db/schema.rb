9937ddfabbdb33e904b66004cf2f920d807b3929
Keith Rozett <krozett@gmail.com>
2015-09-03 17:51:23 -0700

    Issue #233 - Change event description to text type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816190116) do
+ActiveRecord::Schema.define(version: 20150904004827) do
 
   create_table "badge_categories", force: true do |t|
     t.string   "name"
@@ -108,7 +108,7 @@ ActiveRecord::Schema.define(version: 20150816190116) do
   create_table "events", force: true do |t|
     t.integer  "user_id"
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.datetime "start_datetime"
     t.datetime "stop_datetime"
     t.string   "location"
