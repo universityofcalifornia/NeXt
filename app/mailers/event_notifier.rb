8e38667f5f3fd972f136b00f679e4425e75f7082
Keith Rozett <krozett@gmail.com>
2015-10-15 18:14:13 -0700

    Issue #253 - Ignore bad invites

--- app/mailers/event_notifier.rb
+++ app/mailers/event_notifier.rb
@@ -1,9 +1,10 @@
 class EventNotifier < ActionMailer::Base
-  default from: "from@example.com"
+  default from:     "mailer@ucnext.org",
+          reply_to: "no-reply@ucnext.org"
 
   def notify_invite(invite)
     @invite = invite
     @event = invite.event
-    mail(:to => "#{invite.email}", :subject => "Invitation to Event - #{@event.name}")
+    mail(:to => "#{invite.email}", :subject => "Invitation to Event - #{@event.name}") unless @event.nil?
   end
 end
