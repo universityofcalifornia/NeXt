e220032cb7dcd6dd167e0a5682425b6fa3e1dfde
Keith Rozett <krozett@gmail.com>
2015-11-05 17:26:54 -0800

    Issue #31 - Create ES index if it doesn't exist

--- config.ru
+++ config.ru
@@ -1,6 +1,3 @@
-# RELOAD_INDICES: set true to reset all Organization indices and then load in all Polls again (performance intense!)
-RELOAD_INDICES = false
-
 # Load environment
 require ::File.expand_path('../config/environment',  __FILE__)
 
@@ -16,24 +13,6 @@ begin
   Rails.application.config.elasticsearch_client.ping
 rescue => e
   abort "ERROR: Could not connect to Elasticsearch"
-end
-
-# RELOAD INDICES (if configured as such)
-if RELOAD_INDICES
-  [
-    { name: 'idea', class: Idea, label: :name },
-    { name: 'project', class: Project, label: :name },
-    { name: 'user', class: User, label: :display_name }
-  ].each do |o|
-    o[:class].all.each do |obj|
-      if obj.index_exists?
-        puts "[#{o[:name]} - reset index - #{obj.id}] #{obj.send(o[:label])}"
-        obj.destroy_index!
-      end
-      puts "[#{o[:name]} - create index - #{obj.id}] #{obj.send(o[:label])}"
-      obj.create_index!
-    end
-  end
 end
 
 # RUN THE RAILS APPLICATION
--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,11 +3,14 @@
 require 'elasticsearch'
 require 'yaml'
 
+# RELOAD_INDICES: set true to reset all indices (performance intense!)
+RELOAD_INDICES = ENV["RELOAD_INDICES"]
+
 config = {
-    hosts: [
-        { host: 'localhost', port: 9200 }
-    ],
-    index_name: 'next'
+  hosts: [
+    { host: 'localhost', port: 9200 }
+  ],
+  index_name: 'next'
 }
 
 # Use the YAML file to override the host, port and index name
@@ -18,5 +21,29 @@ end
 Rails.application.config.elasticsearch_client = Elasticsearch::Client.new(config)
 
 Rails.application.config.elasticsearch_config = {
-   :index_name => config[:index_name]
+  :index_name => config[:index_name]
 }
+
+# Create the index if it doesn't exist yet
+unless Rails.application.config.elasticsearch_client.indices.exists index: config[:index_name]
+  puts "Creating new elasticsearch index '#{config[:index_name]}'"
+  Rails.application.config.elasticsearch_client.indices.create index: config[:index_name]
+end
+
+# RELOAD INDICES (if configured as such)
+if RELOAD_INDICES
+  [
+    { name: 'idea', class: Idea, label: :name },
+    { name: 'project', class: Project, label: :name },
+    { name: 'user', class: User, label: :display_name }
+  ].each do |o|
+    o[:class].all.each do |obj|
+      if obj.index_exists?
+        puts "[#{o[:name]} - reset index - #{obj.id}] #{obj.send(o[:label])}"
+        obj.destroy_index!
+      end
+      puts "[#{o[:name]} - create index - #{obj.id}] #{obj.send(o[:label])}"
+      obj.create_index!
+    end
+  end
+end
