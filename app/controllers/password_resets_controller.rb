9f94fabe7c4af36fa26380e37444186be80f68b3
shaun <shaun965@hotmail.com>
2015-12-01 18:44:55 -0800

    Completed forgotten/reset password just without email client

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,10 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :authenticated_user,   only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
+
+
 
   def new
   end
@@ -7,10 +13,11 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: password_reset_params[:email].downcase)
     if @user
       @user.create_reset_digest
-      @user.send_password_reset_email
+      #@user.send_password_reset_email
+      redirect_to edit_password_reset_url(@user.reset_token, email: @user.email)
       flash[:page_alert] = "Email sent with password reset instructions"
       flash[:page_alert_type] = "success"
-      redirect_to root_url
+      #redirect_to root_url
     else
       flash[:page_alert] = "<strong>Error.</strong> Email address not found"
       flash[:page_alert_type] = "danger"
@@ -21,10 +28,47 @@ class PasswordResetsController < ApplicationController
   def edit
   end
 
+  def update
+    if params[:user][:password] != params[:user][:password_confirmation]
+      flash[:page_alert] = '<strong>Error.</strong> Password and confirmation did not match. Please try again.'
+      flash[:page_alert_type] = 'danger'
+      render 'edit'
+    elsif !User.valid_password(params[:user][:password])
+      flash[:page_alert] = '<strong>Error.</strong> Password needs to be at least 8 characters long and contain at least one letter and one number'
+      flash[:page_alert_type] = 'danger'
+      render 'edit'
+    elsif @user.update_attributes(password_hash: BCrypt::Password.create(user_params[:password]))
+      flash[:page_alert] = "Password has been reset"
+      flash[:page_alert_type] = "success"
+      redirect_to root_url
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def password_reset_params
     params.require(:password_reset).permit(:email)
+  end
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def authenticated_user
+    redirect_to root_url unless BCrypt::Password.new(@user.reset_digest).is_password?(params[:id])
+  end
+
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -80,6 +80,10 @@ class User < ActiveRecord::Base
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   def send_password_reset_email
     # UserMailer.password_reset(self).deliver_now
   end
