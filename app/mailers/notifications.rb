6c9b74dbed5e33849818843429dc02b273153da2
Keith Rozett <krozett@gmail.com>
2015-12-12 17:56:49 -0800

    Issue #257 - Better design for notification emails

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -3,8 +3,9 @@ class Notifications < ActionMailer::Base
           reply_to: "no-reply@ucnext.org"
 
   def badge_received badge, user
-    @badge = badge
-    @user  = user
+    @badge      = badge
+    @user       = user
+    @link_style = "color: #9c5500;"
 
     mail :to => @user.email, :subject => "You received a badge!"
   end
