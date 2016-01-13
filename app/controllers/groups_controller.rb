a9c71f64555571486113abe90fe21eace8b76c6d
Keith Rozett <krozett@gmail.com>
2016-01-13 14:58:52 -0800

    Issue #312 - Return all groups when searching in the Events form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -55,9 +55,7 @@ class GroupsController < ApplicationController
 
   def ajax_index
     groups = Group.where("name like ?", "%#{params[:q]}%")
-    user_groups = current_user.groups.map(&:id)
-    new_groups = groups.reject{|n| user_groups.include?(n.id)}
-    render json: new_groups.as_json(only: [:id, :name])
+    render json: groups.as_json(only: [:id, :name])
   end
 
   private
