b0c315e3953c23365ca07641bbf21917763291bf
shaun <shaun965@hotmail.com>
2016-02-17 13:38:31 -0800

    Issue #398 added some tests for email

---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe EventsController, type: :controller do
+
+  it "Should return false for bad email input" do
+    @controller = EventsController.new
+    # @controller.instance_eval{ valid_email? 'test1@gmail.com, test2@gmail.com' }.expect == true
+    expect(@controller.instance_eval{ valid_email? 'test1@gmail.comtest2@gmail.com' }).to be false 
+    expect(@controller.instance_eval{ valid_email? 'test1@gmail.com;test2@gmail.com' }).to be false 
+    expect(@controller.instance_eval{ valid_email? 'test1@gmail.com test2@gmail.com' }).to be false 
+    expect(@controller.instance_eval{ valid_email? 'test1@gmail.com.test2@gmail.com' }).to be false 
+
+  end
+
+  it "Should return true for good email input" do
+    @controller = EventsController.new
+    # @controller.instance_eval{ valid_email? 'test1@gmail.com, test2@gmail.com' }.expect == true
+    expect(@controller.instance_eval{ valid_email? 'test1@gmail.com' }).to be true
+    expect(@controller.instance_eval{ valid_email? 'test1@gmail.com, test2@gmail.com' }).to be true
+    expect(@controller.instance_eval{ valid_email? 'test1@gmail.com,test2@gmail.com' }).to be true 
+
+  end
+
+end
