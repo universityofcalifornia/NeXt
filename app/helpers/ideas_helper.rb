ecb3e7af1ed44ae8d76fd9f1fab20c75e033732f
Keith Rozett <krozett@gmail.com>
2016-02-16 18:29:10 -0800

    Issue #334 - Show plain text instead of links for non-viewable projects/ideas

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,9 @@
 module IdeasHelper
+  def link_to_idea idea
+    if current_user && idea.is_viewable_by?(current_user)
+      link_to idea.name, idea
+    else
+      content_tag :span, idea.name, :title => "This idea is campus-specific."
+    end
+  end
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def link_to_project project
+    if current_user && project.is_viewable_by?(current_user)
+      link_to project.name, project
+    else
+      content_tag :span, project.name, :title => "This project is campus-specific."
+    end
+  end
 end
